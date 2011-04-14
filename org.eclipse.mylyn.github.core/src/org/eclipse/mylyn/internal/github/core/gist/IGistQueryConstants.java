533d241e6036eefadfad67872a4242f09354adc6
Kevin Sawicki <kevin@github.com>
2011-04-14 09:59:42 -0700

    Add gist query constants.
    Change-Id: If5a27e4eecd18e0ddbf10b0026d2a3607ad3b8ca
    Signed-off-by: Kevin Sawicki <kevin@github.com>

---
+++ org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/internal/github/core/gist/IGistQueryConstants.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.internal.github.core.gist;
+
+/**
+ * Constants used in gist repositories query properties.
+ * 
+ * @author Kevin Sawicki (kevin@github.com)
+ */
+public interface IGistQueryConstants {
+
+	/**
+	 * USER
+	 */
+	String USER = "user"; //$NON-NLS-1$
+
+}
