8fd8eef06c010d36360d788606159e808d684885
Christian Trutz <christian.trutz@gmail.com>
2011-04-18 23:58:12 +0200

    Label#hashCode() method added
    And better (from Eclipse generated) equals method. This was a FindBugs
    major error.
    
    Change-Id: I4a96db52987055a3d127560da5aff4333afc9d25
    Signed-off-by: Christian Trutz <christian.trutz@gmail.com>

--- org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/github/internal/Label.java
+++ org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/github/internal/Label.java
@@ -22,14 +22,34 @@ public class Label {
 	private String url;
 
 	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/**
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
+	@Override
 	public boolean equals(Object obj) {
-		if (obj == this)
+		if (this == obj)
 			return true;
-
-		return obj instanceof Label && this.name != null
-				&& this.name.equals(((Label) obj).name);
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Label other = (Label) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 	/**
