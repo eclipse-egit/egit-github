0aafe6924156706b84928ce5abd1504073b44d22
Christian Trutz <christian.trutz@gmail.com>
2011-05-10 23:52:34 +0200

    Submit failed: Unexpected error: Unprocessable Entry
    Bug: 345341
    Change-Id: I07fe2a49dbdc47f3cba3f660d263e89f4f4952c9
    Signed-off-by: Christian Trutz <christian.trutz@gmail.com>

--- org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/github/internal/IssueService.java
+++ org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/github/internal/IssueService.java
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.mylyn.github.internal;
 
-import com.google.gson.reflect.TypeToken;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -19,247 +17,242 @@ import java.util.Map;
 
 import org.eclipse.core.runtime.Assert;
 
+import com.google.gson.reflect.TypeToken;
+
 /**
- * Issue service class for listing, searching, and fetching {@link Issue}
- * objects using a {@link GitHubClient}.
+ * Issue service class for listing, searching, and fetching {@link Issue} objects using a
+ * {@link GitHubClient}.
  * 
  * @author Kevin Sawicki (kevin@github.com)
  */
 public class IssueService {
 
-	/**
-	 * Filter by issue assignee
-	 */
-	public static final String FILTER_ASSIGNEE = "assignee"; //$NON-NLS-1$
+    /**
+     * Filter by issue assignee
+     */
+    public static final String FILTER_ASSIGNEE = "assignee"; //$NON-NLS-1$
 
-	/**
-	 * Filter by issue's milestone
-	 */
-	public static final String FILTER_MILESTONE = "milestone"; //$NON-NLS-1$
+    /**
+     * Filter by issue's milestone
+     */
+    public static final String FILTER_MILESTONE = "milestone"; //$NON-NLS-1$
 
-	/**
-	 * Filter by user mentioned in issue
-	 */
-	public static final String FILTER_MENTIONED = "mentioned"; //$NON-NLS-1$
+    /**
+     * Filter by user mentioned in issue
+     */
+    public static final String FILTER_MENTIONED = "mentioned"; //$NON-NLS-1$
 
-	/**
-	 * Filter by issue's labels
-	 */
-	public static final String FILTER_LABELS = "labels"; //$NON-NLS-1$
+    /**
+     * Filter by issue's labels
+     */
+    public static final String FILTER_LABELS = "labels"; //$NON-NLS-1$
 
-	/**
-	 * Filter by issue's state
-	 */
-	public static final String FILTER_STATE = "state"; //$NON-NLS-1$
+    /**
+     * Filter by issue's state
+     */
+    public static final String FILTER_STATE = "state"; //$NON-NLS-1$
 
-	/**
-	 * Issue open state filter value
-	 */
-	public static final String STATE_OPEN = "open"; //$NON-NLS-1$
+    /**
+     * Issue open state filter value
+     */
+    public static final String STATE_OPEN = "open"; //$NON-NLS-1$
 
-	/**
-	 * Issue closed state filter value
-	 */
-	public static final String STATE_CLOSED = "closed"; //$NON-NLS-1$
+    /**
+     * Issue closed state filter value
+     */
+    public static final String STATE_CLOSED = "closed"; //$NON-NLS-1$
 
-	/**
-	 * Issue body field name
-	 */
-	public static final String FIELD_BODY = "body"; //$NON-NLS-1$
+    /**
+     * Issue body field name
+     */
+    public static final String FIELD_BODY = "body"; //$NON-NLS-1$
 
-	/**
-	 * Issue title field name
-	 */
-	public static final String FIELD_TITLE = "title"; //$NON-NLS-1$
+    /**
+     * Issue title field name
+     */
+    public static final String FIELD_TITLE = "title"; //$NON-NLS-1$
 
-	private GitHubClient client;
+    private GitHubClient client;
 
-	/**
-	 * Create issue service
-	 * 
-	 * @param client
-	 *            cannot be null
-	 */
-	public IssueService(GitHubClient client) {
-		Assert.isNotNull(client, "Client cannot be null"); //$NON-NLS-1$
-		this.client = client;
-	}
+    /**
+     * Create issue service
+     * 
+     * @param client cannot be null
+     */
+    public IssueService(GitHubClient client) {
+        Assert.isNotNull(client, "Client cannot be null"); //$NON-NLS-1$
+        this.client = client;
+    }
 
-	/**
-	 * Get issue
-	 * 
-	 * @param user
-	 * @param repository
-	 * @param id
-	 * @return issue
-	 * @throws IOException
-	 */
-	public Issue getIssue(String user, String repository, String id)
-			throws IOException {
-		Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(id, "Id cannot be null"); //$NON-NLS-1$
-		StringBuilder builder = new StringBuilder(
-				IGitHubConstants.SEGMENT_REPOS);
-		builder.append('/').append(user).append('/').append(repository);
-		builder.append(IGitHubConstants.SEGMENT_ISSUES);
-		builder.append('/').append(id).append(IGitHubConstants.SUFFIX_JSON);
-		return this.client.get(builder.toString(), Issue.class);
-	}
+    /**
+     * Get issue
+     * 
+     * @param user
+     * @param repository
+     * @param id
+     * @return issue
+     * @throws IOException
+     */
+    public Issue getIssue(String user, String repository, String id) throws IOException {
+        Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(id, "Id cannot be null"); //$NON-NLS-1$
+        StringBuilder builder = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
+        builder.append('/').append(user).append('/').append(repository);
+        builder.append(IGitHubConstants.SEGMENT_ISSUES);
+        builder.append('/').append(id).append(IGitHubConstants.SUFFIX_JSON);
+        return this.client.get(builder.toString(), Issue.class);
+    }
 
-	/**
-	 * Get an issue's comments
-	 * 
-	 * @param user
-	 * @param repository
-	 * @param id
-	 * @return list of matching issues
-	 * @throws IOException
-	 */
-	public List<Comment> getComments(String user, String repository, String id)
-			throws IOException {
-		Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(id, "Id cannot be null"); //$NON-NLS-1$
-		StringBuilder builder = new StringBuilder(
-				IGitHubConstants.SEGMENT_REPOS);
-		builder.append('/').append(user).append('/').append(repository);
-		builder.append(IGitHubConstants.SEGMENT_ISSUES);
-		builder.append('/').append(id);
-		builder.append(IGitHubConstants.SEGMENT_COMMENTS).append(
-				IGitHubConstants.SUFFIX_JSON);
-		TypeToken<List<Comment>> commentToken = new TypeToken<List<Comment>>() {
-		};
-		return this.client.get(builder.toString(), commentToken.getType());
-	}
+    /**
+     * Get an issue's comments
+     * 
+     * @param user
+     * @param repository
+     * @param id
+     * @return list of matching issues
+     * @throws IOException
+     */
+    public List<Comment> getComments(String user, String repository, String id)
+            throws IOException {
+        Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(id, "Id cannot be null"); //$NON-NLS-1$
+        StringBuilder builder = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
+        builder.append('/').append(user).append('/').append(repository);
+        builder.append(IGitHubConstants.SEGMENT_ISSUES);
+        builder.append('/').append(id);
+        builder.append(IGitHubConstants.SEGMENT_COMMENTS).append(
+                IGitHubConstants.SUFFIX_JSON);
+        TypeToken<List<Comment>> commentToken = new TypeToken<List<Comment>>() {
+        };
+        return this.client.get(builder.toString(), commentToken.getType());
+    }
 
-	/**
-	 * Get a list of {@link Issue} objects that match the specified filter data
-	 * 
-	 * @param user
-	 * @param repository
-	 * @param filterData
-	 * @return list of issues
-	 * @throws IOException
-	 */
-	public List<Issue> getIssues(String user, String repository,
-			Map<String, String> filterData) throws IOException {
-		Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
-		StringBuilder builder = new StringBuilder(
-				IGitHubConstants.SEGMENT_REPOS);
-		builder.append('/').append(user).append('/').append(repository);
-		builder.append(IGitHubConstants.SEGMENT_ISSUES).append(
-				IGitHubConstants.SUFFIX_JSON);
-		TypeToken<List<Issue>> issueToken = new TypeToken<List<Issue>>() {
-		};
-		return this.client.get(builder.toString(), filterData,
-				issueToken.getType());
-	}
+    /**
+     * Get a list of {@link Issue} objects that match the specified filter data
+     * 
+     * @param user
+     * @param repository
+     * @param filterData
+     * @return list of issues
+     * @throws IOException
+     */
+    public List<Issue> getIssues(String user, String repository,
+            Map<String, String> filterData) throws IOException {
+        Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
+        StringBuilder builder = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
+        builder.append('/').append(user).append('/').append(repository);
+        builder.append(IGitHubConstants.SEGMENT_ISSUES).append(
+                IGitHubConstants.SUFFIX_JSON);
+        TypeToken<List<Issue>> issueToken = new TypeToken<List<Issue>>() {
+        };
+        return this.client.get(builder.toString(), filterData, issueToken.getType());
+    }
 
-	/**
-	 * Create issue map for issue
-	 * 
-	 * @param issue
-	 * @return map
-	 */
-	protected Map<String, String> createIssueMap(Issue issue) {
-		Map<String, String> params = new HashMap<String, String>();
-		if (issue != null) {
-			params.put(FIELD_BODY, issue.getBody());
-			params.put(FIELD_TITLE, issue.getTitle());
-			User assignee = issue.getAssignee();
-			if (assignee != null)
-				params.put(FILTER_ASSIGNEE, assignee.getName());
+    /**
+     * Create issue map for issue
+     * 
+     * @param issue
+     * @return map
+     */
+    protected Map<String, String> createIssueMap(Issue issue) {
+        Map<String, String> params = new HashMap<String, String>();
+        if (issue != null) {
+            params.put(FIELD_BODY, issue.getBody());
+            params.put(FIELD_TITLE, issue.getTitle());
+            User assignee = issue.getAssignee();
+            if (assignee != null) {
+                String assigneName = assignee.getName();
+                if (!"".equals(assigneName))
+                    params.put(FILTER_ASSIGNEE, assignee.getName());
+            }
 
-			Milestone milestone = issue.getMilestone();
-			if (milestone != null) {
-				int number = milestone.getNumber();
-				if (number > 0)
-					params.put(FILTER_MILESTONE, Integer.toString(number));
-				else
-					params.put(FILTER_MILESTONE, ""); //$NON-NLS-1$
-			}
-		}
-		return params;
-	}
+            Milestone milestone = issue.getMilestone();
+            if (milestone != null) {
+                int number = milestone.getNumber();
+                if (number > 0)
+                    params.put(FILTER_MILESTONE, Integer.toString(number));
+            }
+        }
+        return params;
+    }
 
-	/**
-	 * Create issue
-	 * 
-	 * @param user
-	 * @param repository
-	 * @param issue
-	 * @return created issue
-	 * @throws IOException
-	 */
-	public Issue createIssue(String user, String repository, Issue issue)
-			throws IOException {
-		Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
-		StringBuilder uri = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
-		uri.append('/').append(user).append('/').append(repository);
-		uri.append(IGitHubConstants.SEGMENT_ISSUES).append(
-				IGitHubConstants.SUFFIX_JSON);
+    /**
+     * Create issue
+     * 
+     * @param user
+     * @param repository
+     * @param issue
+     * @return created issue
+     * @throws IOException
+     */
+    public Issue createIssue(String user, String repository, Issue issue)
+            throws IOException {
+        Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
+        StringBuilder uri = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
+        uri.append('/').append(user).append('/').append(repository);
+        uri.append(IGitHubConstants.SEGMENT_ISSUES).append(IGitHubConstants.SUFFIX_JSON);
 
-		Map<String, String> params = createIssueMap(issue);
-		return this.client.post(uri.toString(), params, Issue.class);
-	}
+        Map<String, String> params = createIssueMap(issue);
+        return this.client.post(uri.toString(), params, Issue.class);
+    }
 
-	/**
-	 * Edit issue
-	 * 
-	 * @param user
-	 * @param repository
-	 * @param issue
-	 * @return created issue
-	 * @throws IOException
-	 */
-	public Issue editIssue(String user, String repository, Issue issue)
-			throws IOException {
-		Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(issue, "Issue cannot be null"); //$NON-NLS-1$
-		StringBuilder uri = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
-		uri.append('/').append(user).append('/').append(repository);
-		uri.append(IGitHubConstants.SEGMENT_ISSUES);
-		uri.append('/').append(issue.getNumber())
-				.append(IGitHubConstants.SUFFIX_JSON);
+    /**
+     * Edit issue
+     * 
+     * @param user
+     * @param repository
+     * @param issue
+     * @return created issue
+     * @throws IOException
+     */
+    public Issue editIssue(String user, String repository, Issue issue)
+            throws IOException {
+        Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(issue, "Issue cannot be null"); //$NON-NLS-1$
+        StringBuilder uri = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
+        uri.append('/').append(user).append('/').append(repository);
+        uri.append(IGitHubConstants.SEGMENT_ISSUES);
+        uri.append('/').append(issue.getNumber()).append(IGitHubConstants.SUFFIX_JSON);
 
-		Map<String, String> params = createIssueMap(issue);
-		String state = issue.getState();
-		if (state != null) {
-			params.put(FILTER_STATE, state);
-		}
-		return this.client.put(uri.toString(), params, Issue.class);
-	}
+        Map<String, String> params = createIssueMap(issue);
+        String state = issue.getState();
+        if (state != null) {
+            params.put(FILTER_STATE, state);
+        }
+        return this.client.put(uri.toString(), params, Issue.class);
+    }
 
-	/**
-	 * Create comment on specified issue id
-	 * 
-	 * @param user
-	 * @param repository
-	 * @param issueId
-	 * @param comment
-	 * @return created issue
-	 * @throws IOException
-	 */
-	public Comment createComment(String user, String repository,
-			String issueId, String comment) throws IOException {
-		Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
-		Assert.isNotNull(issueId, "Issue id cannot be null"); //$NON-NLS-1$
-		StringBuilder uri = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
-		uri.append('/').append(user).append('/').append(repository);
-		uri.append(IGitHubConstants.SEGMENT_ISSUES);
-		uri.append('/').append(issueId);
-		uri.append(IGitHubConstants.SEGMENT_COMMENTS).append(
-				IGitHubConstants.SUFFIX_JSON);
+    /**
+     * Create comment on specified issue id
+     * 
+     * @param user
+     * @param repository
+     * @param issueId
+     * @param comment
+     * @return created issue
+     * @throws IOException
+     */
+    public Comment createComment(String user, String repository, String issueId,
+            String comment) throws IOException {
+        Assert.isNotNull(user, "User cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(repository, "Repository cannot be null"); //$NON-NLS-1$
+        Assert.isNotNull(issueId, "Issue id cannot be null"); //$NON-NLS-1$
+        StringBuilder uri = new StringBuilder(IGitHubConstants.SEGMENT_REPOS);
+        uri.append('/').append(user).append('/').append(repository);
+        uri.append(IGitHubConstants.SEGMENT_ISSUES);
+        uri.append('/').append(issueId);
+        uri.append(IGitHubConstants.SEGMENT_COMMENTS)
+                .append(IGitHubConstants.SUFFIX_JSON);
 
-		Map<String, String> params = new HashMap<String, String>(1, 1);
-		params.put(FIELD_BODY, comment);
+        Map<String, String> params = new HashMap<String, String>(1, 1);
+        params.put(FIELD_BODY, comment);
 
-		return this.client.post(uri.toString(), params, Comment.class);
-	}
+        return this.client.post(uri.toString(), params, Comment.class);
+    }
 
 }
