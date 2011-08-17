01a35ee8d0fbf3f882f5dd493bf584ead9bf16cc
Kevin Sawicki <kevin@github.com>
2011-08-17 12:28:07 -0700

    Support user public keys and user e-mail address APIs
    Change-Id: I9eb60818daa76667c36b1297a4fdb9bd610baffc
    Signed-off-by: Kevin Sawicki <kevin@github.com>

---
+++ org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/client/EntityDeleteMethod.java
@@ -0,0 +1,49 @@
+/******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.egit.github.core.client;
+
+import java.net.URI;
+
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+
+/**
+ *
+ */
+public class EntityDeleteMethod extends HttpEntityEnclosingRequestBase {
+
+	/**
+	 *
+	 */
+	public EntityDeleteMethod() {
+		super();
+	}
+
+	/**
+	 * @param uri
+	 */
+	public EntityDeleteMethod(final URI uri) {
+		super();
+		setURI(uri);
+	}
+
+	/**
+	 * @param uri
+	 */
+	public EntityDeleteMethod(final String uri) {
+		super();
+		setURI(URI.create(uri));
+	}
+
+	public String getMethod() {
+		return HttpDelete.METHOD_NAME;
+	}
+}
