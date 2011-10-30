7615378861425a714a377daed43fc2e7d35d38aa
Kevin Sawicki <kevin@github.com>
2011-10-30 08:57:35 -0700

    Escape '.' characters in search query to '%2E'
    Change-Id: I2fc31f370a2c87fbadbc5523473b855e29d53ad5
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/service/RepositoryService.java
+++ org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/service/RepositoryService.java
@@ -484,7 +484,7 @@ public class RepositoryService extends GitHubService {
 		uri.append(SEGMENT_REPOS);
 		uri.append(SEGMENT_SEARCH);
 		final String encodedQuery = URLEncoder.encode(query, CHARSET_UTF8)
-				.replace("+", "%20");
+				.replace("+", "%20").replace(".", "%2E"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		uri.append('/').append(encodedQuery);
 
 		PagedRequest<SearchRepository> request = createPagedRequest();
