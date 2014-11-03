d2ab8f5a9967b5ad4c7bc7daf2c5bf910ad3e735
mcmathews <mcmathe1@asu.edu>
2014-11-03 14:33:39 -0700

    Implemented the Team Membership API
    Github recently rolled out the Team Membership API,
    and subsequently deprecated the Team Members API.
    Currently, using the Team Members API to add a member
    to a team who is not already part of the org, will result
    in an error.
    The changes made are to implement the new Team
    Memberships API.
    
    Bug: 447419
    Change-Id: I6773e31113688ac027cf8b061107f54a35ab10ff
    Signed-off-by: Michael Mathews <mcmathe1@asu.edu>

--- org.eclipse.egit.github.core.tests/src/org/eclipse/egit/github/core/tests/TeamServiceTest.java
+++ org.eclipse.egit.github.core.tests/src/org/eclipse/egit/github/core/tests/TeamServiceTest.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Copyright (c) 2011 GitHub Inc.
+ *  Copyright (c) 2011 2014 GitHub Inc.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  *  Contributors:
  *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *    Michael Mathews - Team Membership API implementation
  *****************************************************************************/
 package org.eclipse.egit.github.core.tests;
 
@@ -21,6 +22,7 @@ import java.util.Collections;
 
 import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.Team;
+import org.eclipse.egit.github.core.TeamMembership;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.client.GitHubRequest;
 import org.eclipse.egit.github.core.client.GitHubResponse;
@@ -294,6 +296,56 @@ public class TeamServiceTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void removeMemberEmptyName() throws IOException {
 		service.removeMember(3, "");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void getMembershipNullName() throws IOException {
+		service.getMembership(6, null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void getMembershipEmptyName() throws IOException {
+		service.getMembership(6, "");
+	}
+
+	@Test
+	public void getMembership() throws IOException {
+		service.getMembership(6, "tt");
+		GitHubRequest request = new GitHubRequest();
+		request.setUri("/teams/6/memberships/tt");
+		verify(client).get(request);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void addMembershipNullName() throws IOException {
+		service.addMember(6, null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void addMembershipEmptyName() throws IOException {
+		service.addMember(6, "");
+	}
+
+	@Test
+	public void addMembership() throws IOException {
+		service.addMember(6, "tt");
+		verify(client).put("/teams/6/memberships/tt", null, TeamMembership.class);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void removeMembershipNullName() throws IOException {
+		service.removeMember(6, null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void removeMembershipEmptyName() throws IOException {
+		service.removeMember(6, "");
+	}
+
+	@Test
+	public void removeMembership() throws IOException {
+		service.removeMember(6, "tt");
+		verify(client).delete("/teams/6/memberships/tt");
 	}
 
 	/**
