f8c7d4a038f6ea043c858ce0de9b32083de04f26
Matthias Sohn <matthias.sohn@sap.com>
2015-03-05 00:09:15 +0100

    Prevent invalid CommitStatus.state values
    see https://developer.github.com/v3/repos/statuses/
    
    Change-Id: I62f8cbf1e276a3d3da270292153a80028c872b2c
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/CommitStatus.java
+++ org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/CommitStatus.java
@@ -11,6 +11,7 @@
 package org.eclipse.egit.github.core;
 
 import java.io.Serializable;
+import java.text.MessageFormat;
 import java.util.Date;
 
 import org.eclipse.egit.github.core.util.DateUtils;
@@ -150,10 +151,15 @@ public class CommitStatus implements Serializable {
 	/**
 	 * @param state
 	 * @return this status
+	 * throws {@link IllegalArgumentException} if state is invalid
 	 */
 	public CommitStatus setState(final String state) {
-		this.state = state;
-		return this;
+		if (state.equals(STATE_ERROR) || state.equals(STATE_FAILURE) || state.equals(STATE_PENDING)
+				|| state.equals(STATE_SUCCESS)) {
+			this.state = state;
+			return this;
+		}
+		throw new IllegalArgumentException(MessageFormat.format("Invalid state {0}", state));
 	}
 
 	/**
