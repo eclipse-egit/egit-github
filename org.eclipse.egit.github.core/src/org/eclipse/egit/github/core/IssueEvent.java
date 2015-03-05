c358bc39c0a183f888b4abba825cd3609a8ca8fe
Jon Ander Peñalba <jonan88@gmail.com>
2015-03-05 10:51:14 +0100

    Improve the IssueEvent class adding more fields
    Relevant GitHub API docs: https://developer.github.com/v3/issues/events/
    
    Change-Id: I5f4e64558ca3eb010bdd01680c0056676c0f7738
    Signed-off-by: Jon Ander Peñalba <jonan88@gmail.com>

--- org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/IssueEvent.java
+++ org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/IssueEvent.java
@@ -20,150 +20,204 @@ import org.eclipse.egit.github.core.util.DateUtils;
  */
 public class IssueEvent implements Serializable {
 
-	/** serialVersionUID */
-	private static final long serialVersionUID = -842754108817725707L;
+    public static final String TYPE_CLOSED = "closed";
+    public static final String TYPE_REOPENED = "reopened";
+    public static final String TYPE_SUBSCRIBED = "subscribed";
+    public static final String TYPE_MERGED = "merged";
+    public static final String TYPE_REFERENCED = "referenced";
+    public static final String TYPE_MENTIONED = "mentioned";
+    public static final String TYPE_ASSIGNED = "assigned";
+    public static final String TYPE_UNASSIGNED = "unassigned";
+    public static final String TYPE_LABELED = "labeled";
+    public static final String TYPE_UNLABELED = "unlabeled";
+    public static final String TYPE_MILESTONED = "milestoned";
+    public static final String TYPE_DEMILESTONED = "demilestoned";
+    public static final String TYPE_RENAMED = "renamed";
+    public static final String TYPE_LOCKED = "locked";
+    public static final String TYPE_UNLOCKED = "unlocked";
+    public static final String TYPE_HEAD_REF_DELETED = "head_ref_deleted";
+    public static final String TYPE_HEAD_REF_RESTORED = "head_ref_restored";
 
-	private Date createdAt;
+    /** serialVersionUID */
+    private static final long serialVersionUID = -842754108817725707L;
 
-	private Issue issue;
+    private long id;
 
-	private long id;
+    private String url;
 
-	private String commitId;
+    private User actor;
 
-	private String event;
+    private String commitId;
 
-	private String url;
+    private String event;
 
-	private User actor;
+    private Date createdAt;
 
-	private Label label;
+    private Label label;
 
-	/**
-	 * @return createdAt
-	 */
-	public Date getCreatedAt() {
-		return DateUtils.clone(createdAt);
-	}
+    private User assignee;
 
-	/**
-	 * @param createdAt
-	 * @return this issue event
-	 */
-	public IssueEvent setCreatedAt(Date createdAt) {
-		this.createdAt = DateUtils.clone(createdAt);
-		return this;
-	}
+    private Milestone milestone;
 
-	/**
-	 * @return issue
-	 */
-	public Issue getIssue() {
-		return issue;
-	}
+    private Issue issue;
 
-	/**
-	 * @param issue
-	 * @return this issue event
-	 */
-	public IssueEvent setIssue(Issue issue) {
-		this.issue = issue;
-		return this;
-	}
+    /**
+     * @return id
+     */
+    public long getId() {
+        return id;
+    }
 
-	/**
-	 * @return id
-	 */
-	public long getId() {
-		return id;
-	}
+    /**
+     * @param id
+     * @return this issue event
+     */
+    public IssueEvent setId(long id) {
+        this.id = id;
+        return this;
+    }
 
-	/**
-	 * @param id
-	 * @return this issue event
-	 */
-	public IssueEvent setId(long id) {
-		this.id = id;
-		return this;
-	}
+    /**
+     * @return url
+     */
+    public String getUrl() {
+        return url;
+    }
 
-	/**
-	 * @return commitId
-	 */
-	public String getCommitId() {
-		return commitId;
-	}
+    /**
+     * @param url
+     * @return this issue event
+     */
+    public IssueEvent setUrl(String url) {
+        this.url = url;
+        return this;
+    }
 
-	/**
-	 * @param commitId
-	 * @return this issue event
-	 */
-	public IssueEvent setCommitId(String commitId) {
-		this.commitId = commitId;
-		return this;
-	}
+    /**
+     * @return actor
+     */
+    public User getActor() {
+        return actor;
+    }
 
-	/**
-	 * @return event
-	 */
-	public String getEvent() {
-		return event;
-	}
+    /**
+     * @param actor
+     * @return this issue event
+     */
+    public IssueEvent setActor(User actor) {
+        this.actor = actor;
+        return this;
+    }
 
-	/**
-	 * @param event
-	 * @return this issue event
-	 */
-	public IssueEvent setEvent(String event) {
-		this.event = event;
-		return this;
-	}
+    /**
+     * @return commitId
+     */
+    public String getCommitId() {
+        return commitId;
+    }
 
-	/**
-	 * @return url
-	 */
-	public String getUrl() {
-		return url;
-	}
+    /**
+     * @param commitId
+     * @return this issue event
+     */
+    public IssueEvent setCommitId(String commitId) {
+        this.commitId = commitId;
+        return this;
+    }
 
-	/**
-	 * @param url
-	 * @return this issue event
-	 */
-	public IssueEvent setUrl(String url) {
-		this.url = url;
-		return this;
-	}
+    /**
+     * @return event
+     */
+    public String getEvent() {
+        return event;
+    }
 
-	/**
-	 * @return actor
-	 */
-	public User getActor() {
-		return actor;
-	}
+    /**
+     * @param event
+     * @return this issue event
+     */
+    public IssueEvent setEvent(String event) {
+        this.event = event;
+        return this;
+    }
 
-	/**
-	 * @param actor
-	 * @return this issue event
-	 */
-	public IssueEvent setActor(User actor) {
-		this.actor = actor;
-		return this;
-	}
+    /**
+    * @return createdAt
+    */
+    public Date getCreatedAt() {
+        return DateUtils.clone(createdAt);
+    }
 
-	/**
-	 * @return label
-	 */
-	public Label getLabel() {
-		return label;
-	}
+    /**
+    * @param createdAt
+    * @return this issue event
+    */
+    public IssueEvent setCreatedAt(Date createdAt) {
+        this.createdAt = DateUtils.clone(createdAt);
+        return this;
+    }
 
-	/**
-	 * @param label
-	 * @return this issue event
-	 */
-	public IssueEvent setLabel(Label label) {
-		this.label = label;
-		return this;
-	}
+    /**
+     * @return label
+     */
+    public Label getLabel() {
+        return label;
+    }
+
+    /**
+     * @param label
+     * @return this issue event
+     */
+    public IssueEvent setLabel(Label label) {
+        this.label = label;
+        return this;
+    }
+
+    /**
+     * @return actor
+     */
+    public User getAssignee() {
+        return assignee;
+    }
+
+    /**
+     * @param assignee
+     * @return this issue event
+     */
+    public IssueEvent setAssignee(User assignee) {
+        this.assignee = assignee;
+        return this;
+    }
+
+    /**
+     * @return milestone
+     */
+    public Milestone getMilestone() {
+        return milestone;
+    }
+
+    /**
+     * @param milestone
+     * @return this issue event
+     */
+    public IssueEvent setMilestone(Milestone milestone) {
+        this.milestone = milestone;
+        return this;
+    }
+
+    /**
+    * @return issue
+    */
+    public Issue getIssue() {
+        return issue;
+    }
+
+    /**
+    * @param issue
+    * @return this issue event
+    */
+    public IssueEvent setIssue(Issue issue) {
+        this.issue = issue;
+        return this;
+    }
 }
